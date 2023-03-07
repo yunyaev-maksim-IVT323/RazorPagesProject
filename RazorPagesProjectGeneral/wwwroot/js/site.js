bd8812a58090c1f7119451e56240bae9b7f536bf
myunyaev@outlook.com <myunyaev@outlook.com>
2023-03-07 22:31:00 +0500

    Добавил вывод координат картинок на экран и сохранение в массиве.

--- RazorPagesProjectGeneral/wwwroot/js/site.js
+++ RazorPagesProjectGeneral/wwwroot/js/site.js
@@ -2,34 +2,45 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-let bead = document.querySelectorAll('.bead');
-let playa = document.querySelector('#playa');
+let bead = document.querySelectorAll('.bead'); // переменная хранит список элементов с классом bead
+let playa = document.querySelector('#playa'); // переменная хранит элемент playa
+
+
 
 let flag = false, imgClone = null;
 
-for (var elem of bead) elem.addEventListener('mousedown', addRemoveCopy);
+// Для каждого элемента в bead регистрируется обработчик события "нажатие" и запускает функцию
+for (var elem of bead)
+    elem.addEventListener('mousedown', addRemoveCopy)   
 
 function addRemoveCopy(e) {
-    e.preventDefault();
+    e.preventDefault(); // Отменяет действие браузера по умолчанию
     flag = true;
 
     if (this.classList.contains('imgCopy')) {
-        // Если нажали на клон с шифтом удалить. Иначе присвоить переменной imgClone - елемент на котором нажата кнопка мыши
+        // Если нажали на клон с шифтом удалить. Иначе присвоить переменной imgClone - ссылку на объект инициатор события (элемент на котором нажата кнопка мыши)
         return e.shiftKey ? e.target.remove() : imgClone = e.target;
     }
 
-    imgClone = e.target.cloneNode();
-    imgClone.setAttribute('class', 'imgCopy');
-    imgClone.addEventListener('mousedown', addRemoveCopy);
-    playa.append(imgClone);
+    imgClone = e.target.cloneNode();  //Клонируем элемент и получаем его точную копию
+    imgClone.setAttribute('class', 'imgCopy'); // Изменяем значение атрибута клона, присваиваем ему новый класс
+    imgClone.addEventListener('mousedown', addRemoveCopy); // Регистрируем обработчик события "нажатие" и вызываем функцию заново
+
+    playa.append(imgClone); // Вставляем клон в нижнюю зону (зону построения)
 }
+
+
 
 document.addEventListener('mousemove', function (e) {
     if (!flag || !imgClone) return;
     let box = imgClone.getBoundingClientRect();
+
+    let info = document.getElementById("info");
+
     imgClone.style.left = e.pageX - box.width / 2 + 'px';
     imgClone.style.top = e.pageY - box.height / 2 + 'px';
-    imgClone.style.right = e.pageX - box.width / 2 + 'px';
+    imgClone.style.x = box.x;
+    imgClone.style.y = box.y;
     if (imgClone.style.left <= 0 + 'px') {
         imgClone.style.left = 0 + 'px'
     }
@@ -38,14 +49,32 @@ document.addEventListener('mousemove', function (e) {
         imgClone.style.top = 0 + 'px'
     }
 
+    //saveElement(imgClone);
+    info.innerHTML = "Top: " + imgClone.style.top +
+        "<br>Left: " + imgClone.style.left +
+        "<br>X: " + imgClone.style.x + 
+        "<br>Y: " + imgClone.style.y;
 });
 
 document.addEventListener('mouseup', function (e) {
     imgClone = null;
     flag = false;
 });
-$(function () {
-    $('#element').draggable({
-        containment: '.element-wrp'
-    });
-});
+
+
+function saveCoord() {
+    let place = '';
+    //var place = document.getElementById("qqq");
+    let vivod = document.getElementById("vivod");
+    let clons = document.querySelectorAll('.imgCopy'); // Список всех клонов на странице
+    let mas = [];
+    for (var i = 0; i < clons.length; i++) {
+        mas[i] = [clons[i].x, clons[i].y];
+        //place += '<div>' + mas[i] + '</div>';
+        place += '<div>' + "X: " + mas[i][0] + ' '+ "Y: " + mas[i][1] + '</div>';
+        //place += '<div>' + clons[i].x + ' ' + clons[i].y + '</div>';
+        vivod.innerHTML = place;
+        //document.write(clons[i].x, clons[i].y);
+    }
+    //info.innerHTML = place;
+}
